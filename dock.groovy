71f0eddcb4bed0d769bea724140fc5fe70c41a38
karthikpaidi <karthik.paidi@gmail.com>
2016-08-13 13:28:32 -0700

    Create dock.groovy

---
+++ dock.groovy
@@ -0,0 +1,16 @@
+node ('master')
+{
+
+    stage 'delete all existing file'
+    sh 'rm -rf *'
+    stage 'clone git repo'
+    sh 'git clone https://github.com/karthikpaidi/testrrepo1.git'
+    stage 'copy to dockerbuild directory'
+    sh 'cp /var/lib/jenkins/workspace/pipecode/testrrepo1/karthik.html /dockerbuilds'
+    stage 'Remove old docker images'
+    sh 'docker rmi -f finale'
+    stage 'build new docker images'
+    sh 'docker build -t finale /dockerbuilds/.'
+
+
+}
