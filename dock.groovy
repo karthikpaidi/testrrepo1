52c9fe9db1a9660225b62c45314fade42987d18d
karthikpaidi <karthik.paidi@gmail.com>
2016-08-14 13:53:06 -0700

    Update dock.groovy

--- dock.groovy
+++ dock.groovy
@@ -6,7 +6,7 @@ node ('master')
     stage 'clone git repo'
     sh 'git clone https://github.com/karthikpaidi/testrrepo1.git'
     stage 'copy to dockerbuild directory'
-    sh 'cp /var/lib/jenkins/workspace/pipecode/testrrepo1/karthik.html /dockerbuilds'
+    sh 'cp testrrepo1/karthik.html /dockerbuilds'
     stage 'Remove old docker images'
     sh 'docker rmi -f finale'
     stage 'build new docker images'
